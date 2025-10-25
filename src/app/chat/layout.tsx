5a02047c9f55f602bfa90188faad7aa3947ce7cd
stalkiq <almoreau1@outlook.com>
2025-10-25 13:07:21 -0400

    Clean up unused chat APIs and components

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -1,23 +1,11 @@
-'use client';
-import * as React from 'react';
-import { UserProvider } from './user-context';
-
-function ChatLayoutContent({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex h-screen w-full bg-black">
-      <main className="flex h-full flex-1 flex-col">{children}</main>
-    </div>
-  );
-}
-
 export default function ChatLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <UserProvider>
-      <ChatLayoutContent>{children}</ChatLayoutContent>
-    </UserProvider>
+    <div className="flex h-screen w-full bg-black">
+      <main className="flex h-full flex-1 flex-col">{children}</main>
+    </div>
   );
 }
